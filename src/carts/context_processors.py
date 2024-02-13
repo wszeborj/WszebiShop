e90007b7d48b4e66dc885dbf00c5ac3d3f54c49e
wszeborj <wszeborj@o2.pl>
2024-02-14 00:04:09 +0100

    refactor: total quantity cart is downloaded from database, not calculated in python

--- src/carts/context_processors.py
+++ src/carts/context_processors.py
@@ -1,12 +1,18 @@
 from .models import CartItem
+from django.db.models import Sum
 
 
 
 def cart_items_count_processor(request):
-    cart_items = CartItem.objects.filter(account=request.user)
-    cart_total_quantity = 0
-    for item in cart_items:
-        cart_total_quantity += item.quantity
-    context = {'cart_total_quantity': cart_total_quantity}
+    # cart_items = CartItem.objects.filter(account=request.user)
+    # cart_total_quantity = 0
+    # for item in cart_items:
+    #     cart_total_quantity += item.quantity
+    # context = {'cart_total_quantity': cart_total_quantity}
+    context = {'cart_total_quantity': 0}
+    cart_items_quantity = CartItem.objects.filter(account=request.user).aggregate(total=Sum('quantity'))['total']
+
+    if cart_items_quantity is not None:
+        context['cart_total_quantity'] = cart_items_quantity
 
     return context
