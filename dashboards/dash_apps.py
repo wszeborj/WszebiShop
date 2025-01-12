6559a74db63ccd0456a3fde88c23ad7f3165d504
wszeborj <wszeborj@o2.pl>
2025-01-12 16:21:35 +0100

    [feature] added in dashboard filtering of user [fix] in gitignore removed src in paths

--- dashboards/dash_apps.py
+++ dashboards/dash_apps.py
@@ -5,34 +5,44 @@ import plotly.express as px
 from dash import Input, Output, dash_table, dcc, html
 from django_plotly_dash import DjangoDash
 
+from shop.models import Category
+
 from .dash_data import DashData
 
-df = DashData.get_combined_data_by_date()
-df["created_at__date"] = pd.to_datetime(df["created_at__date"])
-
 app = DjangoDash("GenericDash")
+
+category_options = [{"label": "All", "value": ""}] + [
+    {"label": cat.name, "value": cat.id} for cat in Category.objects.all()
+]
+
 app.layout = html.Div(
     [
         html.Div(children="Daily reports"),
         html.Hr(),
         dcc.DatePickerRange(
             id="date-picker-range",
-            min_date_allowed=df["created_at__date"].min(),
+            min_date_allowed=date(2024, 1, 1),
             max_date_allowed=date.today(),
-            start_date=df["created_at__date"].min(),
-            end_date=df["created_at__date"].max(),
+            start_date=date(2024, 1, 1),
+            end_date=date.today(),
             display_format="YYYY-MM-DD",
             clearable=True,
         ),
         dcc.RadioItems(
-            options=[{"label": col, "value": col} for col in df.columns[1:]],
-            value="order_count",
             id="controls-and-radio-item",
+            options=[
+                {"label": "Order Count", "value": "order_count"},
+                {"label": "Total Payment", "value": "total_payment"},
+                {"label": "Total Quantity", "value": "total_quantity"},
+                {"label": "Average Order Value", "value": "avg_order_value"},
+                {"label": "Avg Quantity Per Order", "value": "avg_quantity_per_order"},
+            ],
+            value="order_count",
         ),
         dcc.Graph(
             id="controls-and-graph", style={"width": "100%", "display": "inline-block"}
         ),
-        dash_table.DataTable(id="data-table", data=df.to_dict("records"), page_size=5),
+        dash_table.DataTable(id="data-table", page_size=5),
     ]
 )
 
@@ -44,10 +54,15 @@ app.layout = html.Div(
     Input("date-picker-range", "start_date"),
     Input("date-picker-range", "end_date"),
 )
-def update_output(col_chosen, start_date, end_date):
+def update_output(col_chosen, start_date, end_date, **kwargs):
+    user = kwargs["user"]
+    df = DashData.get_combined_data_by_date(user=user)
+    df["created_at__date"] = pd.to_datetime(df["created_at__date"])
+
     filtered_df = df[
         (df["created_at__date"] >= start_date) & (df["created_at__date"] <= end_date)
     ]
+
     fig = px.bar(filtered_df, x="created_at__date", y=col_chosen)
 
     table = filtered_df.to_dict("records")
--- dashboards/dash_data.py
+++ dashboards/dash_data.py
@@ -6,38 +6,66 @@ from orders.models import Order, OrderItem
 
 class DashData:
     @staticmethod
-    def get_daily_orders():
+    def get_daily_orders(user=None, category=None):
+        orders = Order.objects.all()
+
+        if user and not user.is_staff:
+            orders = orders.filter(buyer_id=user.id)
+
+        if category:
+            orders = orders.filter(order_items__product__category_id=category)
+
         orders = (
-            Order.objects.values("created_at__date")
+            orders.values("created_at__date")
             .annotate(order_count=Count("id"))
             .order_by("created_at__date")
         )
         return pd.DataFrame(orders)
 
     @staticmethod
-    def get_daily_order_items():
+    def get_daily_order_items(user=None, category=None):
+        order_items = OrderItem.objects.all()
+
+        if user and not user.is_staff:
+            order_items = order_items.filter(order__buyer_id=user.id)
+
+        if category:
+            order_items = order_items.filter(product__category_id=category)
+
         order_items = (
-            OrderItem.objects.values("created_at__date")
+            order_items.values("created_at__date")
             .annotate(total_quantity=Sum("quantity"))
             .order_by("created_at__date")
         )
         return pd.DataFrame(order_items)
 
     @staticmethod
-    def get_daily_payments():
+    def get_daily_payments(user=None, category=None):
+        payments = Order.objects.filter(status=Order.OrderStatus.PAID)
+
+        if user and not user.is_staff:
+            payments = payments.filter(buyer_id=user.id)
+
+        if category:
+            payments = payments.filter(order_items__product__category_id=category)
+
         payments = (
-            Order.objects.values("created_at__date")
+            payments.values("created_at__date")
             .annotate(total_payment=Sum("total_price_with_shipping"))
             .order_by("created_at__date")
         )
         return pd.DataFrame(payments)
 
     @staticmethod
-    def get_combined_data_by_date():
-        df_daily_orders = DashData.get_daily_orders()
-        df_daily_order_items = DashData.get_daily_order_items()
-        df_daily_payments = DashData.get_daily_payments()
-        if OrderItem.objects.count() > 0:
+    def get_combined_data_by_date(user=None, category=None):
+        if user.is_staff:
+            user = None
+        df_daily_orders = DashData.get_daily_orders(user=user, category=category)
+        df_daily_order_items = DashData.get_daily_order_items(
+            user=user, category=category
+        )
+        df_daily_payments = DashData.get_daily_payments(user=user, category=category)
+        if not df_daily_orders.empty:
             combined_df = pd.merge(
                 df_daily_orders,
                 df_daily_order_items,
@@ -49,10 +77,10 @@ class DashData:
             )
 
             combined_df["avg_order_value"] = (
-                combined_df["total_payment"] / combined_df["order_count"]
+                combined_df["total_payment"].astype(float) / combined_df["order_count"]
             )
             combined_df["avg_quantity_per_order"] = (
-                combined_df["total_quantity"] / combined_df["order_count"]
+                combined_df["total_quantity"].astype(float) / combined_df["order_count"]
             )
 
             combined_df = combined_df.fillna(0)
